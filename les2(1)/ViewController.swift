581d5487dc105a659a08585ae9503fbc5d7f4aa2
PavelSkripov <74505136+PavelSkripov@users.noreply.github.com>
2020-11-17 21:17:03 +0500

    дз2.1

---
+++ les2(1)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  les2(1)
+//
+//  Created by Павел on 17.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
