e35dcc81895ad4b825d69bbf5d0597f0466ed82b
PavelSkripov <74505136+PavelSkripov@users.noreply.github.com>
2020-11-18 15:23:40 +0500

    + new branch

---
+++ les2(1)/newView.swift
@@ -0,0 +1,20 @@
+//
+//  newView.swift
+//  les2(1)
+//
+//  Created by Павел on 18.11.2020.
+//
+
+import UIKit
+
+class newView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
